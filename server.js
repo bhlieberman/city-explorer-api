acd646f126e44fc2fb186799fb4a1edcf429ae0a
Ben Lieberman <60364696+bhlieberman@users.noreply.github.com>
2022-09-27 14:27:41 -0600

    backend setup complete

---
+++ server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// const forecast = require('./forecast.js');
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const reduce = require('lodash.reduce');
+// Read in the shopping list from our "Database" (flat file)
+const data = require('./data/weather.json');
+
+const app = express();
+
+// middleware
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+class Forecast {
+    constructor(date, desc) {
+        this.date = date;
+        this.description = desc;
+    }
+}
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+app.get('/', (req, res) => {
+    res.send("Hello from Express");
+})
+
+app.get('/weather', (req, res) => {
+    let { lat, lon, searchQuery } = req.query;
+    let response = reduce(data, (acc, city) => {
+        if (city.city_name === searchQuery && city.lat === lat && city.lon === lon) {
+            let data = city.data.map(data => new Forecast(data.datetime, data.weather.description));
+            acc = acc.concat(data);
+        }
+        return acc;
+    }, []);
+    if (response) res.send(response);
+    else res.status(404).send('not found');
+})
