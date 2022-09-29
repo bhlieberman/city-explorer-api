af5979f6c45b3acade82b7d2bcc5060ad0b128fb
Ben Lieberman <60364696+bhlieberman@users.noreply.github.com>
2022-09-28 22:47:36 -0600

    lab 08 backend complete

---
+++ movies.js
@@ -0,0 +1,18 @@
+class Movie {
+    constructor(
+        overview,
+        popularity,
+        poster_path,
+        release_date,
+        title,
+        vote_average,
+        vote_count) {
+        this.overview = overview
+        this.popularity = popularity
+        this.poster_path = poster_path
+        this.release_date = release_date
+        this.title = title
+        this.vote_average = vote_average
+        this.vote_count = vote_count
+    }
+}
